8c6de2f293f5cb3dee6f2a87c953ce3c57df6431
ca-m <117754457+ca-m@users.noreply.github.com>
2023-01-23 10:30:51 +0200

    Update convert_number.c

--- convert_number.c
+++ convert_number.c
@@ -4,9 +4,9 @@
  * print_hex - prints unsigned hex numbers in lowercase
  * @ap: the argument pointer
  * @params: the parameters struct
- *
  * Return: bytes printed
  */
+
 int print_hex(va_list ap, params_t *params)
 {
 	unsigned long l;
